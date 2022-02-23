388a752e308ae110c19b71471131efe3daa16fd6
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-23 22:52:12 +0900

    [Fix] package.json script

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./index";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
