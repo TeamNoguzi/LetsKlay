be313fa6431c338f75fb2613f4caf89734d3c062
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-24 15:17:09 +0900

    [Fix] storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,15 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
