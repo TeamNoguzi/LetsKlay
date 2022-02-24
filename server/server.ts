71d47bb3b4a1896bde28f20531d3a1e0ebe3644c
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-24 14:22:34 +0900

    [Fix] server config + sequelize

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./index";
 
-const port = process.env.PORT || 5000;
+const port = process.env.NODE_ENV === 'production'? 3000 : 5000;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
