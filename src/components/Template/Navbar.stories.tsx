2494aab563763f735036328b7d0d6fcbe2b780ab
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-25 16:12:39 +0900

    [Add] template

---
+++ src/components/Template/Navbar.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Navbar, {NavbarProps} from './Navbar';
+
+export default {
+    title: 'Navbar',
+    component: Navbar,
+} as ComponentMeta<typeof Navbar>;
+
+const Template:ComponentStory<typeof Navbar> = args => <Navbar {...args}/>;
+
+export const Default = Template.bind({});
+Default.args = {
+}
---
+++ src/components/Template/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Navbar.module.css';
+
+interface NavbarProps {
+    
+}
+
+const Header:React.FC<NavbarProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Header;
+export type {NavbarProps};
