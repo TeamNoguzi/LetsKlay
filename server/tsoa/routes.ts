49a9eb2fcd52512fe79345a2107ca91e668cac41
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-25 15:05:43 +0900

    [Fix] gitignore

--- server/tsoa/routes.ts
+++ server/tsoa/routes.ts
@@ -3,7 +3,7 @@
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import { Controller, ValidationService, FieldErrors, ValidateError, TsoaRoute, HttpStatusCodeLiteral, TsoaResponse } from '@tsoa/runtime';
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-import { UsersController } from '../routes/Users/usersController';
+import { UsersController } from './../routes/Users/usersController';
 import * as express from 'express';
 
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
