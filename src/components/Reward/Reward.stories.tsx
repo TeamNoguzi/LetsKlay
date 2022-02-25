8e06313c0586f394b305d3443bd6583125221e64
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-25 10:48:25 +0900

    [Fix] reward

--- src/components/Reward/Reward.stories.tsx
+++ src/components/Reward/Reward.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Reward, {RewardProps} from './Reward';
 
+import styles from './Reward.module.css';
+
 export default {
     title: 'Reward',
     component: Reward,
@@ -11,8 +13,22 @@ const Template:ComponentStory<typeof Reward> = args => <Reward {...args}/>;
 
 export const Default = Template.bind({});
 Default.args = {
-    title: 'reward',
-    options: ['hi'],
+    price: 0,
+    title: 'Reward Title',
+    options: [
+        {
+            option: 'Delicious burger',
+            quantity: 1,
+        },
+        {
+            option: 'French Fries',
+            quantity: 1,
+        },
+        {
+            option: 'Zero coke',
+            quantity: 1,
+        }
+    ],
     delievery: {
         start: new Date(),
         end: new Date()
--- src/components/Reward/Reward.tsx
+++ src/components/Reward/Reward.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
 
+import styles from './Reward.module.css';
+
 interface RewardProps {
+    price:number,
     title:string,
-    options:string[],
+    options: {
+        option: string,
+        quantity: number,
+    }[],
     delievery: {
         start:Date,
         end:Date,
@@ -13,17 +19,28 @@ interface RewardProps {
 }
 
 const Reward:React.FC<RewardProps> = 
-({title, options, delievery, maxStock, stock}) => {
+({price, title, options, delievery, maxStock, stock}) => {
 
     return (
-        <Card>
+        <Card className={styles['reward-card']}>
+            <div className={styles['reward-mask']}>
+                Select this reward
+            </div>
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{price} klay</Card.Title>
+                <h4>{title}</h4>
+
+                <h6 className="text-muted">This option gives you a lot of pleasure</h6>
+                <p className="text-small mb-0">includes:</p>
+                <ul>
+                    {options.map(option => <li>{`${option.option} x ${option.quantity}`}</li>)}
+                </ul>
+
+                <p className="text-small mb-0">estimated delievery:</p>
+                <p>{delievery.start.toISOString()} ~ {delievery.end.toISOString()}</p>
+
+                <p className="text-small mb-0">in stock:</p>
+                <p>{`${stock} left out of ${maxStock}`}</p>
             </Card.Body>
         </Card>
     )
