5a8ad6020da1a02d832b9e98341a43cb98d424db
again7536 <45936642+again7536@users.noreply.github.com>
2022-02-26 15:33:25 +0900

    [Add] navigation bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import Navbar from '../Navbar/Navbar';
+import Logo from '../Logo/Logo';
+
 import styles from './Header.module.css';
 
 interface HeaderProps {
-    
+    device:'pc'|'mobile';
 }
 
-const Header:React.FC<HeaderProps> = () => {
+const Header:React.FC<HeaderProps> = ({device}) => {
 
     return (
-        <>
+        <>  
+            <div className={`${styles[`Logo-container-${device}`]}`}>
+                <Logo/>
+            </div>
+            <Navbar type='header' device={device}/>
         </>
     )
 }
