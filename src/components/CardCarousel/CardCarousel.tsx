9c1769450432b028327304521d45c5dd78bdf5ef
BaldwinIV <ksanghun10@gmail.com>
2022-03-01 16:53:20 +0900

    [Add] CardCarousel Component Folders

---
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './CardCarousel.module.css';
+
+interface _CarouselProps {
+    
+}
+
+const CardCarousel:React.FC<_CarouselProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default CardCarousel;
+export type {_CarouselProps};
