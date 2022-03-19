ba3dfa212f72d9ce5046841679a5254605c4d351
BaldwinIV <ksanghun10@gmail.com>
2022-03-19 16:34:58 +0900

    fix : slidetoslide값 변경(하드코딩) Refs:issue#37

--- src/components/CardCarousel/CardCarousel.tsx
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -33,12 +33,12 @@ const CardCarousel: React.FC<CarouselProps> = ({toRenderCardProps}) => {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 4,
-      slidesToSlide: 1, // optional, default to 1.
+      slidesToSlide: 4, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 1, // optional, default to 1.
+      slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
