9182c1102bcba63b3bd8f07abcc5b1eedf2344a6
again7536 <45936642+again7536@users.noreply.github.com>
2022-09-05 12:58:14 +0900

    feat: add reward card

---
+++ src/stories/Cards/RewardCard/index.tsx
@@ -0,0 +1,39 @@
+import { Reward } from "@/entities/rewards";
+import numeral from "numeral";
+import moment from "moment";
+import * as S from "./styled";
+
+interface RewardCardProps {
+  reward: Reward;
+}
+
+const RewardCard = ({ reward }: RewardCardProps) => {
+  return (
+    <S.RewardCard>
+      <S.RewardCardMask>select this reward</S.RewardCardMask>
+      <h1>Support {numeral(reward.price).format("$0,0")}</h1>
+      <h2>{reward.title}</h2>
+      <p>{reward.description}</p>
+      <S.Label>Includes:</S.Label>
+      <ol type="A">
+        {reward.items &&
+          reward.items.map((item) => (
+            <li key={item.id}>
+              {item.name} x {item.quantity}
+            </li>
+          ))}
+      </ol>
+      <S.Label>Estimated Delievery</S.Label>
+      <p>
+        {moment(reward.deliveryStart).format("YYYY/MM/DD")}~
+        {moment(reward.deliveryEnd).format("YYYY/MM/DD")}
+      </p>
+      <S.Label>In Stock</S.Label>
+      <p>
+        {reward.stock} left out of {reward.maxStock}
+      </p>
+    </S.RewardCard>
+  );
+};
+
+export default RewardCard;
---
+++ src/stories/Cards/RewardCard/styled.tsx
@@ -0,0 +1,62 @@
+import styled from "@emotion/styled";
+import { flexBox } from "styles/mixins";
+
+const RewardCard = styled.div`
+  position: relative;
+
+  width: 100%;
+  max-width: 350px;
+
+  padding: 20px;
+  border: 1px solid ${(props) => props.theme.colors.blackLight}20;
+
+  h1 {
+    margin-bottom: 15px;
+    font-weight: bolder;
+    font-size: 20pt;
+  }
+
+  h2 {
+    font-size: 18pt;
+  }
+
+  p {
+    margin-top: 0;
+    color: #2c2c2cf0;
+
+    &:last-child {
+      margin: 0;
+    }
+  }
+`;
+
+const RewardCardMask = styled.div`
+  ${flexBox({ direction: "column", middle: true })}
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  z-index: 1;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.colors.primaryLight};
+  opacity: 0;
+  transition: 0.1s linear;
+
+  color: ${(props) => props.theme.colors.secondaryBold};
+  font-size: 20pt;
+  font-weight: bold;
+
+  &:hover {
+    opacity: 0.75;
+  }
+`;
+
+const Label = styled.div`
+  font-size: 11pt;
+  color: #2c2c2cb0;
+`;
+
+export { RewardCard, RewardCardMask, Label };
