51fcf890abdd92610880e2a1dfdb53fd198648e5
again7536 <45936642+again7536@users.noreply.github.com>
2022-09-17 13:57:29 +0900

    feat: add ToastUI Editor and descriptions form

--- src/api/images/index.ts
+++ src/api/images/index.ts
@@ -1,7 +1,7 @@
 import axios from "../axios";
 
 const uploadImage = async (image: File) => {
-  const { data } = await axios.post(
+  const { data } = await axios.post<string>(
     "/images",
     { image },
     {
