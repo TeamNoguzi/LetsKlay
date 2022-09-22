43232b20073284b07ca9538c398a2bd57eb959a4
again7536 <45936642+again7536@users.noreply.github.com>
2022-09-22 13:25:10 +0900

    feat: add event entity

---
+++ common/entities/event.ts
@@ -0,0 +1,18 @@
+interface ContractEvent<T> {
+  address: string;
+  blockNumber: number;
+  transactionHash: string;
+  transactionIndex: number;
+  blockHash: string;
+  logIndex: number;
+  id: number;
+  returnValues: T;
+  event: string;
+  signature: string;
+  raw: {
+    data: string;
+    topics: string[];
+  };
+}
+
+export { ContractEvent };
