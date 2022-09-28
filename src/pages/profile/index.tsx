5353f61d62b63ad8083c189e7c752d99cac76ed4
again7536 <45936642+again7536@users.noreply.github.com>
2022-09-28 13:14:39 +0900

    fix: add suspense for fallback

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -9,15 +9,18 @@ import Footer from "stories/Layout/Footer";
 import dynamic from "next/dynamic";
 import Tabs from "stories/Tabs";
 import { useUser } from "hooks";
+import { useState, Suspense } from "react";
 
 const DynamicFundList = dynamic(() => import("sections/Profile/FundList"), { suspense: true });
 const DynamicLikedList = dynamic(() => import("sections/Profile/LikedList"), { suspense: true });
+const DynamicMyProjects = dynamic(() => import("sections/Profile/MyProjects"), { suspense: true });
 
 interface ProfileProps {
   initialUser: FindUserDto;
 }
 
 const Profile = ({ initialUser }: ProfileProps) => {
+  const [tabKey, setTabKey] = useState<string>("fundList");
   const { user } = useUser(initialUser);
 
   return (
@@ -29,12 +32,21 @@ const Profile = ({ initialUser }: ProfileProps) => {
             <MyInfo user={user} />
           </Col>
           <Col>
-            <Tabs defaultActiveKey="FundList">
-              <Tab title="Fund List" eventKey="FundList">
-                <DynamicFundList />
+            <Tabs id="listTab" activeKey={tabKey} onSelect={(key) => setTabKey(key)}>
+              <Tab title="Fund List" eventKey="fundList">
+                <Suspense fallback="loading...">
+                  <DynamicFundList />
+                </Suspense>
               </Tab>
-              <Tab title="Liked Projects" eventKey="LikedProjectList">
-                <DynamicLikedList />
+              <Tab title="My Projects" eventKey="myProjects">
+                <Suspense fallback="loading...">
+                  <DynamicMyProjects />
+                </Suspense>
+              </Tab>
+              <Tab title="Liked Projects" eventKey="likedProjects">
+                <Suspense fallback="loading...">
+                  <DynamicLikedList />
+                </Suspense>
               </Tab>
             </Tabs>
           </Col>
