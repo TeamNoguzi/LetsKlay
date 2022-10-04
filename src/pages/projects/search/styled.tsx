a28db87525941421e22426ec3eec5f469b77b30d
again7536 <45936642+again7536@users.noreply.github.com>
2022-10-04 15:42:28 +0900

    feat: add project search page

---
+++ src/pages/projects/search/styled.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 35px;
+`;
+
+export { PaginationWrapper };
