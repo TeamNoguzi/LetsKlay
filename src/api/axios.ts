41a138a47a977c593ac8e857704e725ddbee5214
again7536 <45936642+again7536@users.noreply.github.com>
2022-10-14 12:03:51 +0900

    fix: production configs

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api"
+      : "https://shorecrabs.site/api",
   withCredentials: true,
 });
 
